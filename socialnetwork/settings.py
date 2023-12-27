1f87b7e73d33adbb823f790b3e3166264c92c4ef
Manisha152 <manishakumari3010cse@gmail.com>
2023-12-27 08:40:22 +0530

    adding project

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-1tkgl7ohbg29^-2(pw^+y4&dx&sknu4&kd^y%k%_t)7xouk($_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+
 
 
 # Application definition
@@ -160,3 +161,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
